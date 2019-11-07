bb3e11c1a37ff64ab0460cfb1fadac07eb582a29
t.vladimir <vovatatosyan2000@mail.ru>
2019-11-07 15:03:05 +0300

    ispravil na pravilnyi raboty so spiskom

--- first_prog/main_var1_1.c
+++ first_prog/main_var1_1.c
@@ -9,63 +9,70 @@ typedef struct list
 }st;
 
 
-void list_bubble_sort(st* head, int size);
-void print_list(st* tmp, int size);
-void clear_list(st* tmp, int size);
+st* list_bubble_sort(st* head);
+void print_list(st* tmp);
+void clear_list(st* tmp);
 
 
 int main(void){
-    int err, num, cnt = 0;
-    st* head = NULL;
-    while ((err = scanf("%d", &num)) != EOF && err != 0) {
-        cnt++;
-        st* tmp = (st*) malloc(sizeof(st));
-        tmp->num = num;
-        tmp->next = head;
-        head = tmp;
-    }
-    list_bubble_sort(head, cnt);
-    st* tmp = head;
-    print_list(tmp, cnt);
-    clear_list(head, cnt);
-    return 0;
+  int err, num;
+  st* head = NULL;
+  while ((err = scanf("%d", &num)) != EOF && err != 0) {
+    st* tmp = (st*) malloc(sizeof(st));
+    tmp->num = num;
+    tmp->next = head;
+    head = tmp;
+  }
+  head = list_bubble_sort(head);
+  st* tmp = head;
+  print_list(tmp);
+  clear_list(head);
+  return 0;
 }
 
 
-void list_bubble_sort(st* head, int size){
-    int i, j, k;
-    for (i = 0; i < size - 1; i++){
-        st* tmp = head;
-        for(j = 0; j < size - i - 1; j++){
-            if (tmp->num > tmp->next->num) {
-                k = tmp->num;
-                tmp->num = tmp->next->num;
-                tmp->next->num = k;
-            }
-            tmp = tmp->next;
-        }
+st* list_bubble_sort(st* head){
+  st* tmp;
+  st* tmp_k;
+  st* resault = head;
+  head = head->next;
+  resault->next = NULL;
+  while(head != NULL){
+    tmp = head;
+    head = head->next;
+    if (tmp->num < resault->num) {
+      tmp->next = resault;
+      resault = tmp;
+    }else{
+      tmp_k = resault;
+      while(tmp_k->next != NULL){
+    if(tmp_k->next->num > tmp->num)
+      break;
+    tmp_k=tmp_k->next;
+      }
+      tmp->next = tmp_k->next;
+      tmp_k->next = tmp;
     }
+  }
+  return resault;
 }
 
 
-void print_list(st* tmp, int size)
+void print_list(st* tmp)
 {
-    int i;
-    for (i=0; i < size; i++){
+    while(tmp !=  NULL){
         printf("%d ", tmp->num);
-        tmp = tmp-> next;
+        tmp = tmp->next;
     }
     printf("\n");
 }
 
 
-void clear_list(st* tmp, int size)
+void clear_list(st* tmp)
 {
-    int i;
-    for(i=0; i<size; i++)
-    {
-        st* tmp_cl = tmp->next;
-        free(tmp);
-        tmp = tmp_cl;
-    }
+  while(tmp != NULL){
+    st* tmp_cl = tmp->next;
+    free(tmp);
+    tmp = tmp_cl;
+  }
 }
