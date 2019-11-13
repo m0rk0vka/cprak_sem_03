ca4c599d091d257a19bf8a73b7fa7989c36a0337
t.vladimir <vovatatosyan2000@mail.ru>
2019-11-14 00:17:34 +0300

    tmp - zavisaet'

--- second_prog/main_var_2.c
+++ second_prog/main_var_2.c
@@ -6,38 +6,53 @@
 #include <stdio.h>
 
 
+void sighndlr(int s);
+
+
 int main(int argc, char** argv){
-    //if (argc < 4){
-    if(fork() == 0){
-        int fd = open(argv[3], O_WRONLY|O_CREAT|O_TRUNC, 0666);
-        if (fd == -1){
-            perror("");
-            return 1;
-        }
-        dup2(fd, 1);
-        close(fd);
-        execlp(argv[1], argv[1], argv[2], (char *)0);
-    }
-    else{
-        int status;
-        wait(&status);
-        if(WIFEXITED(status) && WEXITSTATUS(status)==0){
-            if(fork() == 0){
-                execlp(argv[4], argv[4], (char*)0);
+    signal(SIGTERM, sighndlr);
+    if (argc >= 5){
+        if(fork() == 0){
+            int fd = open(argv[3], O_WRONLY|O_CREAT|O_TRUNC, 0666);
+            if (fd == -1){
+                perror("");
+                return 1;
             }
-            else{
-                int status;
-                wait(&status);
-                if(WIFEXITED(status) && !WEXITSTATUS(status)){
+            dup2(fd, 1);
+            close(fd);
+            execlp(argv[1], argv[1], argv[2], (char *)0);
+        } else{
+            int status;
+            wait(&status);
+            if(WIFEXITED(status) && WEXITSTATUS(status)==0){
+                int fd[2];
+                pipe(fd);
+                if (fork() == 0)
+                {
+                    dup2(fd[1], 1);
+                    execlp(argv[4], argv[4], (char*)0);
+                }
+                else if (fork() == 0){
+                    close(fd[1]);
+                    char * str = NULL;
+                    scanf("%s", str);
+                    printf("%s", str);
+                    dup2(fd[0], 0);
                     execvp(argv[5], argv+5);
                 }
+                wait(0);
+                close(fd[1]);
+                wait(0);
             }
         }
-    }
-    /*}
-    else{
+    } else{
+        printf("argc = %d\n", argc);
         printf("Few arguments were given to the command line!\n");
         return 1;
-    }*/
+    }
     return 0;
+}
+
+void sighndlr(int s){
+
 }
--- second_prog/pr3.c
+++ second_prog/pr3.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 
 int main(int argc, char ** argv){
-    printf("Pain!\n");
+    printf("lloolololo");
+    char * str = NULL;
+    scanf("%s", str);
+    printf("%s", str);
+    int i;
+    for(i = 1; i < argc; i++){
+        printf("%s\n", argv[i]);
+    }
     return 0;
 }
