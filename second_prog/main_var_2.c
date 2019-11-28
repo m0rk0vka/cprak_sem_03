b4b2dee010e68edeee62c6ccb836b09f3e2d6b00
t.vladimir <vovatatosyan2000@mail.ru>
2019-11-28 20:24:51 +0300

    look at this

--- second_prog/main_var_2.c
+++ second_prog/main_var_2.c
@@ -16,8 +16,7 @@ int main(int argc, char** argv){
     printf("Ppid = %d\n", getpid());
     signal(SIGTERM, sighndlr);
     if (argc >= 5){
-        if(fork() == 0){
-            fork_1 = getpid();
+        if((fork_1 = fork()) == 0){
             int fd = open(argv[3], O_WRONLY|O_CREAT|O_TRUNC, 0666);
             if (fd == -1){
                 perror("");
@@ -32,25 +31,23 @@ int main(int argc, char** argv){
             if(WIFEXITED(status) && WEXITSTATUS(status)==0){
                 int fd[2];
                 pipe(fd);
-                if (fork() == 0)
+                if ((fork_2 = fork()) == 0)
                 {
-                    fork_2 = getpid();
                     dup2(fd[1], 1);
                     close(fd[1]);
                     close(fd[0]);
                     execlp(argv[4], argv[4], (char*)0);
                 }
-                else if (fork() == 0){
-                    fork_3 = getpid();
+                else if ((fork_3 = fork()) == 0){
                     close(fd[1]);
                     dup2(fd[0], 0);
                     close(fd[0]);
                     execvp(argv[5], argv+5);
                 }
-                wait(0);
                 close(fd[1]);
-                wait(0);
                 close(fd[0]);
+                wait(0);
+                wait(0);
             }
         }
     } else{
@@ -63,15 +60,15 @@ int main(int argc, char** argv){
 
 void sighndlr(int s){
     if (fork_1 != 0){
-        kill(s, fork_1);
+        kill(fork_1, s);
         fork_1 = 0;
     }
     if (fork_2 != 0){
-        kill(s, fork_2);
+        kill(fork_2, s);
         fork_2 = 0;
     }
     if (fork_3 != 0){
-        kill(s, fork_3);
+        kill(fork_3, s);
         fork_3 = 0;
     }
     exit(0);
