e76b3f2c196244ec6edd1116e5bb14524a81e282
m0rk0vka <55158293+m0rk0vka@users.noreply.github.com>
2019-12-12 14:48:35 +0300

    update main.c
    dfas

--- third_prog/main.c
+++ third_prog/main.c
@@ -5,17 +5,25 @@ int main(int argc, char ** argv){
     int count_sellers = argv[1];
     int count_customers = argv[2];
     int i;
-    for (i = 0; i < count_sellers; i++){
-        if (fork() == 0){
-            execl("./seller", "./seller", (char *) 0);
-            _exit(1);
-        }
-    }
+    /*if (fork() == 0){
+        execl("./seller", "./seller", (char *) 0);
+        _exit(1);
+    }*/
     for (i = 0; i < count_customers; i++){
-        if (fork() == 0){
-            execl("./customer", "./customer", (char *) 0);
-            _exit(1);
+        char * str = "file_";
+        char * c = i + '0';
+        strcat(str, c);
+        FILE *f = fopen(str, "w");
+        int j;
+        for (j = 0; j < count_sellers; j++){
+            int next_in_list = j;
+            fputc(next_in_list, f);
         }
+        fclose(f);
+        /*if (fork() == 0){
+            execl("./customer", "./customer", str, (char *) 0);
+            _exit(1);
+        }*/
     }
     return 0;
 }
