14639bd3eea3f26e8674772983316bf79ec3ea58
t.vladimir <vovatatosyan2000@mail.ru>
2019-12-16 14:05:51 +0300

    dal rysy noyt

--- third_prog/customers.c
+++ third_prog/customers.c
@@ -15,31 +15,45 @@ int main(int argc, char ** argv){
         struct sembuf sops;
         //нужен ли массив семафоров на очередь для изменения разделяемой памяти
         for (list_i = 3; list_i < argc; list_i++){
-            printf("%s\n", argv[list_i]);
             key_t key = ftok("./test", atoi(argv[list_i]));
-            /* создание массива семафоров из одного элемента */
-            semid = semget(key, 1, 0666 | IPC_CREAT);
+            //printf("pokypatel' %s podklychaetsya po key = %d\n", argv[2], key);
+            /* создание массива семафоров */
+            semid = semget(key, atoi(argv[1]) + 2, 0);
             /* создание сегмента разделяемой памяти */
-            shmid = shmget(key, atoi(argv[1]) * sizeof (int), 0666 |IPC_CREAT);
+            shmid = shmget(key, atoi(argv[1]) * sizeof (int), 0);
             /*встаём в очередь*/
-            turn_p = shmat(shmid, NULL, 0);
-            sops.sem_num = 0;
+            turn_p = shmat(shmid, 0, 0);
             sops.sem_flg = 0;
+            sops.sem_num = 0;
             sops.sem_op = -1;
+            printf("1\n");
             semop(semid, &sops, 1);
-            printf("pokupatel' %s vstal v ochered' v %s otdel\n", argv[2], argv[list_i]);
+            printf("2\n");
             i = 0;
             while (turn_p[i] != 0){
                 i++;
             }
+            printf("3\n");
             turn_p[i] = atoi(argv[2]);
+            printf("4\n");
+            sops.sem_num = 1;
+            sops.sem_op = 1;
+            printf("5\n");
+            semop(semid, &sops, 1);
+            printf("6\n");
+            sops.sem_num = 0;
+            sops.sem_op = 1;
+            printf("7\n");
+            semop(semid, &sops, 1);
+            printf("pokupatel' %s vstal v ochered' v %s otdel\n", argv[2], argv[list_i]);
+            //vyhod iz ocheredi
+            sops.sem_num = atoi(argv[2]) + 1;
+            sops.sem_op = -1;
+            semop(semid, &sops, 1);
+            shmdt(turn_p);    //отсоединить сегмент разделяемой памяти;
+            sops.sem_num = atoi(argv[2]) + 1;
             sops.sem_op = 1;
             semop(semid, &sops, 1);
-            //printf("pokupatel' %s vstal v ochered' v %s otdel\n", argv[2], argv[list_i]);
-            while(turn_p[0] != atoi(argv[2])){
-                sleep(1);//kak pravil'no vyiti iz ocheredi
-            }
-            shmdt(turn_p);    //отсоединить сегмент разделяемой памяти;
             sleep(1);
         }
         printf("pokupatel' %s vyshel iz magazina\n", argv[1]);
--- third_prog/sellers.c
+++ third_prog/sellers.c
@@ -22,30 +22,42 @@ int main(int argc, char ** argv){
     int * turn_p;
     struct sembuf sops;
     key_t key = ftok("./test", atoi(argv[1]));
+    printf("otdel %s sozdaet key = %d\n", argv[1], key);
+    printf("argv[2]=%s\n", argv[2]);
     /* создание массива семафоров из 1 элемента */
-    semid = semget(key, 1, 0666 | IPC_CREAT);
+    semid = semget(key, atoi(argv[2]) + 2, 0666 | IPC_CREAT);
     /* создание сегмента разделяемой памяти */
     shmid = shmget(key, atoi(argv[2])*sizeof(int), 0666 | IPC_CREAT);
-    turn_p = shmat(shmid, NULL, 0);
-    semctl(semid, 0, SETVAL, (int) 0);
-    sops.sem_num = 0;
+    turn_p = shmat(shmid, 0, 0);
+    int i;
+    printf("lolllllllll\n");
+    for (i = 0; i < atoi(argv[2]); i++){
+        printf("pamat': i=%d, turn_p[i]=%d\n", i, turn_p[i]);
+        turn_p[i] = 0;
+    }
+    printf("kaak\n");
     sops.sem_flg = 0;
-    printf("creat %s otdel\n", argv[1]);
+    semctl(semid, 0, SETVAL, (int) 1); //0-й семафор для пользования очередью
     while(1){
         if (flag == 0){
-            printf("*\n");
-            if (turn_p[0] != 0){
-                sops.sem_op = -1;
-                semop(semid, &sops, 1);
-                printf("ya %s obslyjivay sledyshego\n", argv[1]);
-                int j = 0;
-                while (turn_p[j] != 0){
-                    turn_p[j] = turn_p[j+1];
-                    j++;
-                }
-                sops.sem_op = 1;
-                semop(semid, &sops, 1);
+            sops.sem_num = 1;
+            sops.sem_op = -1;
+            semop(semid, &sops, 1);
+            printf("ya %s obslyjivay sledyshego pod nomerom = %d\n", argv[1], turn_p[0]);
+            int j = 0;
+            sops.sem_num = 0;
+            sops.sem_op = -1;
+            semop(semid, &sops, 1);
+            sops.sem_num = turn_p[0] + 1;
+            while (turn_p[j+1] != 0){
+                turn_p[j] = turn_p[j+1];
+                j++;
             }
+            sops.sem_op = 1;
+            semop(semid, &sops, 1);
+            sops.sem_num = 0;
+            sops.sem_op = 1;
+            semop(semid, &sops, 1);
             sleep(1);
         }
         else
